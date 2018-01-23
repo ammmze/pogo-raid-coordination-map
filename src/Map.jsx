5eda4f51f593bb7378025cf119fe1f9f1c67a8cb
Branden <ammmze@gmail.com>
2018-01-22 21:08:52 -0700

    Initial commit

---
+++ src/Map.jsx
@@ -0,0 +1,60 @@
+
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+
+const DEFAULT_VIEWPORT = {
+  center: [33.307771, -111.7738944],
+  zoom: 17,
+}
+
+const markerIcon = {
+  iconUrl: require('https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png'),
+  shadowUrl: require('https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png'),
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76]// point from which the popup should open relative to the iconAnchor
+}
+
+export default class ViewportExample extends Component {
+  state = {
+    viewport: DEFAULT_VIEWPORT,
+    lat: DEFAULT_VIEWPORT.center[0],
+    lng: DEFAULT_VIEWPORT.center[1],
+    zoom: DEFAULT_VIEWPORT.zoom,
+  }
+
+  onClickReset = () => {
+    this.setState({ viewport: DEFAULT_VIEWPORT })
+  }
+
+  onViewportChanged = viewport => {
+    this.setState({ viewport })
+  }
+ 
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map
+        onClick={this.onClickReset}
+        onViewportChanged={this.onViewportChanged}
+        viewport={this.state.viewport}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={position} icon={markerIcon}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
