ea2be5ea0ce94951ba98e3413acb6d2b9c75032a
Branden <ammmze@gmail.com>
2018-01-23 09:45:51 -0700

    removed template stuff. getting familiar with the basic map events

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,9 @@
 
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { BLUE_PIN } from './markers';
 import './Map.css';
 
 const DEFAULT_VIEWPORT = {
@@ -9,15 +11,6 @@ const DEFAULT_VIEWPORT = {
   zoom: 17,
 }
 
-const markerIcon = {
-  iconUrl: require('./img/marker-icon.png'),
-  shadowUrl: require('./img/marker-shadow.png'),
-  iconSize: [38, 95], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor: [-3, -76]// point from which the popup should open relative to the iconAnchor
-}
 
 export default class ViewportExample extends Component {
   state = {
@@ -34,12 +27,35 @@ export default class ViewportExample extends Component {
   onViewportChanged = viewport => {
     this.setState({ viewport })
   }
+
+  handleClick = e => {
+    this.setState({
+      hasLocation: true,
+      lat: e.latlng.lat,
+      lng: e.latlng.lng
+    })
+  }
+
+  jumpToUsersLocation = () => {
+    this.map.leafletElement.locate();
+  }
+
+  handleLocationFound = e => {
+    this.setState({
+      hasLocation: true,
+      lat: e.latlng.lat,
+      lng: e.latlng.lng,
+      viewport: { ...this.state.viewport, center: [e.latlng.lat, e.latlng.lng]}
+    })
+  }
  
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
       <Map
-        onClick={this.onClickReset}
+        ref={(r) => this.map = r}
+        onClick={this.handleClick}
+        onLocationfound={this.handleLocationFound}
         onViewportChanged={this.onViewportChanged}
         viewport={this.state.viewport}>
         <TileLayer
@@ -47,7 +63,7 @@ export default class ViewportExample extends Component {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <Marker position={position}>
+        <Marker position={position} icon={BLUE_PIN}>
           <Popup>
             <span>
               A pretty CSS3 popup. <br /> Easily customizable.
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import Map from './components/Map/Map';
-
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center'
-};
+import './index.css';
 
 const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
-    <Map />
-  </div>
+  <Map />
 );
 
 render(<App />, document.getElementById('root'));
